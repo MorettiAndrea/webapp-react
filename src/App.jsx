a3f963a9ec4f11f69bee752b8b60fd54d8b92bb6
Andrea Moretti <andrea.moretti1704@gmail.com>
2025-06-17 23:11:12 +0200

    minor fixes in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import DefaultLayout from "./layout/defaultLayout";
 
 // pages import
 import MovieList from "./pages/HomePage";
-import MoviesDetails from "./pages/MoviesDetails";
+import MovieDetail from "./pages/MoviesDetails";
 
 
 
@@ -21,7 +21,7 @@ export default function App() {
         <Routes>
           <Route element = {<DefaultLayout />}>
           <Route path={paths.homePage()} element={<MovieList />} />
-          <Route path={paths.MoviesDetails()} element={<MoviesDetails />} />
+          <Route path={paths.MoviesDetails()} element={<MovieDetail />} />
         </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/MoviesDetails.jsx
+++ src/pages/MoviesDetails.jsx
@@ -1,5 +1,5 @@
 
-export default function SearchedMoviePage () 
+export default function MovieDetail () 
 
 { return (
   
